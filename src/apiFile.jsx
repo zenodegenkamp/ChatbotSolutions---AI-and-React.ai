b4a722aa192c20f0255a2a2b7214a69da97d47ca
zenodegenkamp <zenodegenkamp@hotmail.com>
2023-09-25 13:29:46 +0200

    change

--- src/apiFile.jsx
+++ src/apiFile.jsx
@@ -1,33 +1,43 @@
 import { useState } from "react"
 // import { process } from '/env';
-import OpenAI from 'openai';
+// import OpenAI from 'openai';
 
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-  dangerouslyAllowBrowser: true
-})
+// const openai = new OpenAI({
+//   apiKey: process.env.OPENAI_API_KEY,
+//   dangerouslyAllowBrowser: true
+// })
 
 
 
 export default async function FetchChatBot(converstationStr) {
 
-  const url 
+  const url = 'chatbotsolutions.netlify/.netlify/functions/fetchAi/fetchAi.js'
+    
+  const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+          'content-type': 'text/plain',
+      },
+      body: conversationStr
+  })
+  const data = await response.json()
+  console.log(data)
 
-  try {
-    const completion = await openai.completions.create({
-      model: "davinci:ft-personal-2023-08-25-09-01-18",
-      prompt: converstationStr,
-      presence_penalty: 0,
-      frequency_penalty: 0.3,
-      max_tokens: 100,
-      temperature: 0, 
-      stop: ['\n', '->']
-    })
+  // try {
+  //   const completion = await openai.completions.create({
+  //     model: "davinci:ft-personal-2023-08-25-09-01-18",
+  //     prompt: converstationStr,
+  //     presence_penalty: 0,
+  //     frequency_penalty: 0.3,
+  //     max_tokens: 100,
+  //     temperature: 0, 
+  //     stop: ['\n', '->']
+  //   })
 
-    return completion.choices[0].text;
-  } catch (error) {
-    console.error("Error fetching chatbot response:", error);
-    return "Error fetching response";
-  }
+  //   return completion.choices[0].text;
+  // } catch (error) {
+  //   console.error("Error fetching chatbot response:", error);
+  //   return "Error fetching response";
+  // }
 }
 
